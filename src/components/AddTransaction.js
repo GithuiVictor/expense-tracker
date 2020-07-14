10041c3dc69c422a2c1c3ea2801f53dc7c854666
GithuiVictor <victormaina23@gmail.com>
2020-07-14 13:49:24 +0300

    First commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
+    const { addTransaction } = useContext(GlobalContext);
+
+    const onSubmit = e => {
+        e.preventDefault();
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTransaction)
+    }
+    
+    return (
+        <React.Fragment>
+            <h3>Add new transaction</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-control">
+                    <label htmlFor="text">Text</label>
+                    <input type="text" placeholder="Enter text..." name='transactionName' value={text} onChange={e => setText(e.target.value)}/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount">
+                        Amount <br />
+                        (negative - expense, positive - income)
+                    </label>
+                    <input type="number" placeholder="Enter amount..." name='amount' value={amount} onChange={e => setAmount(e.target.value)}/>
+                </div>
+                <button className="btn" type='submit'>Add transaction</button>
+            </form>
+        </React.Fragment>
+    )
+}
---
+++ src/components/Transaction.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+export const Transaction = (props) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+    const sign = props.transaction.amount < 0 ? '-' : '+';
+
+    return (
+        <React.Fragment>
+            <li className={props.transaction.amount < 0 ? 'minus' : 'plus'}>
+                {props.transaction.text} <span>{sign}${Math.abs(props.transaction.amount)}</span>
+                <button className="delete-btn" onClick={()=>deleteTransaction(props.transaction.id)}>x</button>
+            </li>
+        </React.Fragment>
+    )
+}
