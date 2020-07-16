7b74db1e1b9285331d1ac07e0032e00307a7a71a
GithuiVictor <victormaina23@gmail.com>
2020-07-16 16:16:52 +0300

    Updates

---
+++ precache-manifest.f4d6003bdcc73174faf0f9ad249da397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc157bc6203467a10740e3d9d0e62d5",
+    "url": "/Expense-Tracker-/index.html"
+  },
+  {
+    "revision": "5db86b4b15c48f7848f6",
+    "url": "/Expense-Tracker-/static/css/main.cb96896f.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/Expense-Tracker-/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker-/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db86b4b15c48f7848f6",
+    "url": "/Expense-Tracker-/static/js/main.e0a34722.chunk.js"
+  },
+  {
+    "revision": "5594635d31d7f403440e",
+    "url": "/Expense-Tracker-/static/js/runtime-main.b19ea2ad.js"
+  }
+]);
